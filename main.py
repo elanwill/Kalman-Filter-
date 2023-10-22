bc75213bfad71581dca75858955d9ec366f89ef2
heinz <willti@web.de>
2023-10-22 13:59:29 +0200

    first draft of polyPredictFilter, working fine

--- main.py
+++ main.py
@@ -5,20 +5,22 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     dt = 0.01
     T = 10
-    ex = 0.01
+    ex = 0.05
     tvec, x, v, a = drawCurve(T, dt)
     xM = measure(x, ex)
 
     z = 9
     #q = polyPred(tvec[:z],xM[:z],2, graphOut = True)
 
-    xPolyFiltered, vPoly, aPoly = polyFilter(tvec, xM,2,40)
+    xPolyFiltered, vPoly = polyFilter(tvec, xM,2,10)
+
+    print('std measurement ', np.std(xM-x))
+    print('std polypredict ', np.std(xPolyFiltered-x))
 
     plt.figure()
     plt.plot(tvec, x)
     plt.plot(tvec, xM)
     plt.plot(tvec, xPolyFiltered)
-    plt.plot(tvec, vPoly)
-    plt.plot(tvec, aPoly/2)
+    #plt.plot(tvec, vPoly)
     #plt.plot(tvec, xPolyFiltered-xM)
     plt.show()
--- polyFilter.py
+++ polyFilter.py
@@ -23,7 +23,7 @@ def fitPoly(x, y, O, graphOut = False):
 
 def polyPred(x,y,O, graphOut = False):
     if y.size<2: #if y only 1 value predict same value
-        return x, 0, 0
+        return x, 0
     z = fitPoly(x,y,O, graphOut = False)
     p = np.poly1d(z)
     xPred = 2*x[-1]-x[-2]
@@ -36,7 +36,7 @@ def polyPred(x,y,O, graphOut = False):
 
         plt.plot(x, y)
         plt.plot(xPlot, p(xPlot))
-    return (yPred, z[-3], z[-2])
+    return (yPred, z[-2])
 
 def polyFilter(x,y,O,N):
     yFiltered = np.zeros(x.size)
@@ -44,6 +44,6 @@ def polyFilter(x,y,O,N):
     a= np.zeros(x.size)
     for k in range(x.size):
         istart = max([0,k-N])
-        yFiltered[k], v[k], a[k] = polyPred(x[istart:k+1],y[istart:k+1],O)
+        yFiltered[k], v[k] = polyPred(x[istart:k+1],y[istart:k+1],O)
 
-    return yFiltered, v, a
+    return yFiltered, v
